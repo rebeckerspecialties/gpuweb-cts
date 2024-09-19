7db1576ae53b3ffb1cd6a9e22d46843839ff881e
Henry Allen <hmallen99@gmail.com>
2024-09-19 12:38:15 -0400

    Add React Native Test App

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['@babel/preset-typescript'],
+    presets: ['@babel/preset-typescript', 'module:@react-native/babel-preset'],
     plugins: [
       'const-enum',
       [
