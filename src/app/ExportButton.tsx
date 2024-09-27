d0fd5dd79965308bfcc80eabcce4a335acee547f
Henry Allen <hmallen99@gmail.com>
2024-09-27 16:55:50 -0400

    Export file as JSON

--- src/app/ExportButton.tsx
+++ src/app/ExportButton.tsx
@@ -2,12 +2,24 @@ import { useCallback } from 'react';
 import { CTSResults } from './TestTree';
 import { Button } from 'react-native';
 import Share from 'react-native-share';
+import FetchBlob from 'rn-fetch-blob';
 
 export const ExportButton: React.FC<{ results: CTSResults }> = ({ results }) => {
   const onExport = useCallback(() => {
-    Share.open({
-      message: 'sharing results',
-    });
+    const filePath = `${FetchBlob.fs.dirs.DocumentDir}/cts-results.json`;
+
+    FetchBlob.fs
+      .writeFile(filePath, JSON.stringify(results), 'utf8')
+      .then(() =>
+        Share.open({
+          message: 'sharing results',
+          url: filePath,
+        })
+      )
+      .catch(err => {
+        console.warn('Failed to export CSV');
+        console.error(err);
+      });
   }, [results]);
 
   return <Button title="Export results as JSON" onPress={onExport} />;
